bb37247dafa229c1b3646968618e441d6d7f7f5e
Mostafa Emam <mostafaemam19597@gmail.com>
2022-07-18 09:59:33 +0200

    Create Counting-Valleys.cpp

---
+++ Counting-Valleys.cpp
@@ -0,0 +1,13 @@
+int countingValleys(int steps, string path) {
+    int reference=0,down=0;
+    int num_valleys=0;
+    for(int i=0;i<path.size();i++){
+        if(path[i]=='U')reference++;
+        if(path[i]=='D')reference--;
+        
+        if(reference==0 && path[i]=='U' ){
+            num_valleys++;
+        }
+    }
+    return num_valleys;
+}
